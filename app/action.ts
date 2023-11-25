be892aec780ac21db53a08e9b4ee79e06533d4e4
somramnani <som.ramnani@gmail.com>
2023-11-25 15:20:18 -0500

    remove console.log

--- app/action.ts
+++ app/action.ts
@@ -4,8 +4,6 @@ export const fetchAnime = async (page: number) => {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
   );
-
   const data = await response.json();
-  console.log(data);
   return data;
 };
